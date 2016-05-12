4c4c09341407e97d3e844a4e2e2f44c28ca40cc4
John Moon <john.moon@livingsocial.com>
2016-05-12 16:00:29 -0400

    Initial React setup

---
+++ app/config/routes.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+
+var HelloWorld = require('../components/HelloWorld');
+
+var routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={HelloWorld}>
+      <IndexRoute component={HelloWorld} />
+    </Route>
+  </Router>
+);
+
+module.exports = routes;
